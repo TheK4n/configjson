1d6a17e69a2bb070bba9cfdea4bac536677364d9
Pendosv <djvlad967891@gmail.com>
2021-05-18 14:25:36 +0300

    upd: time_tests.py

--- test/time_tests.py
+++ test/time_tests.py
@@ -2,28 +2,11 @@ from timeit import timeit
 
 from ConfigJson import *
 
+cfg = configjson()
+
 
 def main():
-
-    d = {'z': 2,
-         'az': 3123,
-         'b': 121243,
-         'a': 1,
-         'pin': 3,
-         'hash': 'asd',
-         'xuy': 16}
-    cfg = configjson()
-
-    cfg(
-        z=2,
-        az=3123,
-        b=121243,
-        a=1,
-        pin=3,
-        hash=1124,
-        xuy=16)
-
-    print(timeit("test = configjson()", number=100_000, globals=globals()))
+    print(timeit("cfg = configjson()", number=100_000, globals=globals()))
     print(timeit("cfg(z=2, az=3123, b=121243, a=1, pin=3, hash=1124, xuy=16)", number=100_000, globals=globals()))
     print(timeit("cfg.get()", number=100_000, globals=globals()))
     print(timeit("cfg", number=100_000, globals=globals()))
